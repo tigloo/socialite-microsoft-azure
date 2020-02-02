1976c93a03ac4e39105ee5d53baba41e894e6940
tigloo <Till.gerken@gmail.com>
2020-02-02 23:22:28 +0100

    Add files via upload

---
+++ Provider.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace SocialiteProviders\Azure;
+
+use SocialiteProviders\Manager\OAuth2\User;
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'AZURE';
+
+    /**
+     * The base Azure Graph URL.
+     *
+     * @var string
+     */
+    protected $graphUrl = 'https://graph.microsoft.com/v1.0/me';
+
+    /**
+     * The Graph API version for the request.
+     *
+     * @var string
+     */
+    protected $version = '1.0';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://login.microsoftonline.com/'.($this->config['tenant'] ?: 'common').'/oauth2/authorize', $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://login.microsoftonline.com/common/oauth2/token';
+    }
+
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'form_params' => $this->getTokenFields($code),
+        ]);
+
+        $this->credentialsResponseBody = json_decode($response->getBody(), true);
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->graphUrl, [
+            'query' => [
+                //'api-version' => $this->version,
+            ],
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['id'], 'nickname' => null, 'name' => $user['displayName'],
+            'email' => $user['userPrincipalName'], 'avatar' => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+            'resource' => 'https://graph.microsoft.com',
+        ]);
+    }
+
+    /**
+     * Add the additional configuration key 'tenant' to enable the branded sign-in experience.
+     *
+     * @return array
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['tenant'];
+    }
+}
